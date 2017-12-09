f3427c94e3f1fd3949b1b969fa47f2dc0497f0fd
EvilPerfectionist <EvilPerfectionist@users.noreply.github.com>
2017-12-09 22:03:51 +0800

    2017.9.12.5.19 C++ code already

--- TestOpenCV/TestOpenCV/Test.cpp
+++ TestOpenCV/TestOpenCV/Test.cpp
@@ -1,107 +1,224 @@
 #include "opencv2/opencv.hpp"
 #include <iostream>
+#include <cstring>
+
 using namespace cv;
 using namespace std;
-
-//Global Variables
-Mat img, placeholder;
-
-// Callback function for any event on he mouse
-void onMouse(int event, int x, int y, int flags, void* userdata)
-{
-	if (event == EVENT_MOUSEMOVE)
-	{
-
-		Vec3b bgrPixel(img.at<Vec3b>(y, x));
-
-		Mat3b hsv, ycb, lab;
-		// Create Mat object from vector since cvtColor accepts a Mat object
-		Mat3b bgr(bgrPixel);
-
-		//Convert the single pixel BGR Mat to other formats
-		cvtColor(bgr, ycb, COLOR_BGR2YCrCb);
-		cvtColor(bgr, hsv, COLOR_BGR2HSV);
-		cvtColor(bgr, lab, COLOR_BGR2Lab);
-
-		//Get back the vector from Mat
-		Vec3b hsvPixel(hsv.at<Vec3b>(0, 0));
-		Vec3b ycbPixel(ycb.at<Vec3b>(0, 0));
-		Vec3b labPixel(lab.at<Vec3b>(0, 0));
-
-		// Create an empty placeholder for displaying the values
-		placeholder = Mat::zeros(img.rows, 400, CV_8UC3);
-
-		//fill the placeholder with the values of color spaces
-		putText(placeholder, format("BGR [%d, %d, %d]", bgrPixel[0], bgrPixel[1], bgrPixel[2]), Point(20, 70), FONT_HERSHEY_COMPLEX, .9, Scalar(255, 255, 255), 1);
-		putText(placeholder, format("HSV [%d, %d, %d]", hsvPixel[0], hsvPixel[1], hsvPixel[2]), Point(20, 140), FONT_HERSHEY_COMPLEX, .9, Scalar(255, 255, 255), 1);
-		putText(placeholder, format("YCrCb [%d, %d, %d]", ycbPixel[0], ycbPixel[1], ycbPixel[2]), Point(20, 210), FONT_HERSHEY_COMPLEX, .9, Scalar(255, 255, 255), 1);
-		putText(placeholder, format("LAB [%d, %d, %d]", labPixel[0], labPixel[1], labPixel[2]), Point(20, 280), FONT_HERSHEY_COMPLEX, .9, Scalar(255, 255, 255), 1);
+// global variable to keep track of
+bool show = false;
 
 
-		Size sz1 = img.size();
-		Size sz2 = placeholder.size();
-
-		//Combine the two results to show side by side in a single image
-		Mat combinedResult(sz1.height, sz1.width + sz2.width, CV_8UC3);
-		Mat left(combinedResult, Rect(0, 0, sz1.width, sz1.height));
-		img.copyTo(left);
-		Mat right(combinedResult, Rect(sz1.width, 0, sz2.width, sz2.height));
-		placeholder.copyTo(right);
-		imshow("PRESS P for Previous, N for Next Image", combinedResult);
-	}
+// Create a callback for event on trackbars
+void onTrackbarActivity(int pos, void* userdata) {
+	// Just uodate the global variable that there is an event 
+	show = true;
+	return;
 }
 
-int main(int argc, const char** argv)
+
+int main(int argc, char **argv)
 {
-	// filename
-	// Read the input image
 	int image_number = 0;
 	int nImages = 10;
-
 	/*if (argc > 1)
-		nImages = atoi(argv[1]);
-	*/
+		nImages = atoi(argv[1]);*/
 	char filename[20];
 	sprintf_s(filename, "images/rub%02d.jpg", image_number%nImages);
-	img = imread(filename);
-	// Resize the image to 400x400
-	Size rsize(400, 400);
-	resize(img, img, rsize);
 
-	/*if (img.empty())
-	{
-		return -1;
-	}
-	*/
-	// Create an empty window
-	namedWindow("PRESS P for Previous, N for Next Image", WINDOW_AUTOSIZE);
-	// Create a callback function for any event on the mouse
-	setMouseCallback("PRESS P for Previous, N for Next Image", onMouse);
+	Mat original = imread(filename);
 
-	imshow("PRESS P for Previous, N for Next Image", img);
-	while (1)
-	{
-		char k = waitKey(1) & 0xFF;
-		if (k == 27)
-			break;
+	// image resize width and height 
+	int resizeHeight = 250;
+	int resizeWidth = 250;
+	Size rsize(resizeHeight, resizeWidth);
+	resize(original, original, rsize);
+
+	// position on the screen where the windows start 
+	int initialX = 50;
+	int	initialY = 50;
+
+	// creating windows to display images 
+	namedWindow("P-> Previous, N-> Next", WINDOW_AUTOSIZE);
+	namedWindow("SelectBGR", WINDOW_AUTOSIZE);
+	namedWindow("SelectHSV", WINDOW_AUTOSIZE);
+	namedWindow("SelectYCB", WINDOW_AUTOSIZE);
+	namedWindow("SelectLAB", WINDOW_AUTOSIZE);
+
+	// moving the windows to stack them horizontally 
+	moveWindow("P-> Previous, N-> Next", initialX, initialY);
+	moveWindow("SelectBGR", initialX + 1 * (resizeWidth + 5), initialY);
+	moveWindow("SelectHSV", initialX + 2 * (resizeWidth + 5), initialY);
+	moveWindow("SelectYCB", initialX + 3 * (resizeWidth + 5), initialY);
+	moveWindow("SelectLAB", initialX + 4 * (resizeWidth + 5), initialY);
+
+	// creating trackbars to get values for YCrCb 
+	createTrackbar("CrMin", "SelectYCB", 0, 255, onTrackbarActivity);
+	createTrackbar("CrMax", "SelectYCB", 0, 255, onTrackbarActivity);
+	createTrackbar("CbMin", "SelectYCB", 0, 255, onTrackbarActivity);
+	createTrackbar("CbMax", "SelectYCB", 0, 255, onTrackbarActivity);
+	createTrackbar("YMin", "SelectYCB", 0, 255, onTrackbarActivity);
+	createTrackbar("YMax", "SelectYCB", 0, 255, onTrackbarActivity);
+
+	// creating trackbars to get values for HSV 
+	createTrackbar("HMin", "SelectHSV", 0, 180, onTrackbarActivity);
+	createTrackbar("HMax", "SelectHSV", 0, 180, onTrackbarActivity);
+	createTrackbar("SMin", "SelectHSV", 0, 255, onTrackbarActivity);
+	createTrackbar("SMax", "SelectHSV", 0, 255, onTrackbarActivity);
+	createTrackbar("VMin", "SelectHSV", 0, 255, onTrackbarActivity);
+	createTrackbar("VMax", "SelectHSV", 0, 255, onTrackbarActivity);
+
+	// creating trackbars to get values for BGR 
+	createTrackbar("BMin", "SelectBGR", 0, 255, onTrackbarActivity);
+	createTrackbar("BMax", "SelectBGR", 0, 255, onTrackbarActivity);
+	createTrackbar("GMin", "SelectBGR", 0, 255, onTrackbarActivity);
+	createTrackbar("GMax", "SelectBGR", 0, 255, onTrackbarActivity);
+	createTrackbar("RMin", "SelectBGR", 0, 255, onTrackbarActivity);
+	createTrackbar("RMax", "SelectBGR", 0, 255, onTrackbarActivity);
+
+	// creating trackbars to get values for LAB 
+	createTrackbar("LMin", "SelectLAB", 0, 255, onTrackbarActivity);
+	createTrackbar("LMax", "SelectLAB", 0, 255, onTrackbarActivity);
+	createTrackbar("AMin", "SelectLAB", 0, 255, onTrackbarActivity);
+	createTrackbar("AMax", "SelectLAB", 0, 255, onTrackbarActivity);
+	createTrackbar("BMin", "SelectLAB", 0, 255, onTrackbarActivity);
+	createTrackbar("BMax", "SelectLAB", 0, 255, onTrackbarActivity);
+
+	// show all images initially 
+	imshow("SelectHSV", original);
+	imshow("SelectYCB", original);
+	imshow("SelectLAB", original);
+	imshow("SelectBGR", original);
+
+	// declare local variables
+	int BMin, GMin, RMin;
+	int BMax, GMax, RMax;
+	Scalar minBGR, maxBGR;
+
+	int HMin, SMin, VMin;
+	int HMax, SMax, VMax;
+	Scalar minHSV, maxHSV;
+
+	int LMin, aMin, bMin;
+	int LMax, aMax, bMax;
+	Scalar minLab, maxLab;
+
+	int YMin, CrMin, CbMin;
+	int YMax, CrMax, CbMax;
+	Scalar minYCrCb, maxYCrCb;
+
+	Mat imageBGR, imageHSV, imageLab, imageYCrCb;
+	Mat maskBGR, maskHSV, maskLab, maskYCrCb;
+	Mat resultBGR, resultHSV, resultLab, resultYCrCb;
+
+	char k;
+	while (1) {
+		imshow("P-> Previous, N-> Next", original);
+		k = waitKey(1) & 0xFF;
 		//Check next image in the folder
 		if (k == 'n')
 		{
 			image_number++;
 			sprintf_s(filename, "images/rub%02d.jpg", image_number%nImages);
-			img = imread(filename);
-			resize(img, img, rsize);
-			imshow("PRESS P for Previous, N for Next Image", img);
+			original = imread(filename);
+			resize(original, original, rsize);
+			show = true;
 		}
 		//Check previous image in he folder
 		else if (k == 'p')
 		{
 			image_number--;
 			sprintf_s(filename, "images/rub%02d.jpg", image_number%nImages);
-			img = imread(filename);
-			resize(img, img, rsize);
-			imshow("PRESS P for Previous, N for Next Image", img);
+			original = imread(filename);
+			resize(original, original, rsize);
+			show = true;
+		}
+
+		// Close all windows when 'esc' key is pressed		
+		if (k == 27) {
+			break;
+		}
+
+		if (show) { //If there is any event on the trackbar
+			show = false;
+
+			// Get values from the BGR trackbar
+			BMin = getTrackbarPos("BMin", "SelectBGR");
+			GMin = getTrackbarPos("GMin", "SelectBGR");
+			RMin = getTrackbarPos("RMin", "SelectBGR");
+
+			BMax = getTrackbarPos("BMax", "SelectBGR");
+			GMax = getTrackbarPos("GMax", "SelectBGR");
+			RMax = getTrackbarPos("RMax", "SelectBGR");
+
+			minBGR = Scalar(BMin, GMin, RMin);
+			maxBGR = Scalar(BMax, GMax, RMax);
+
+			// Get values from the HSV trackbar
+			HMin = getTrackbarPos("HMin", "SelectHSV");
+			SMin = getTrackbarPos("SMin", "SelectHSV");
+			VMin = getTrackbarPos("VMin", "SelectHSV");
+
+			HMax = getTrackbarPos("HMax", "SelectHSV");
+			SMax = getTrackbarPos("SMax", "SelectHSV");
+			VMax = getTrackbarPos("VMax", "SelectHSV");
+
+			minHSV = Scalar(HMin, SMin, VMin);
+			maxHSV = Scalar(HMax, SMax, VMax);
+
+			// Get values from the LAB trackbar
+			LMin = getTrackbarPos("LMin", "SelectLAB");
+			aMin = getTrackbarPos("AMin", "SelectLAB");
+			bMin = getTrackbarPos("BMin", "SelectLAB");
+
+			LMax = getTrackbarPos("LMax", "SelectLAB");
+			aMax = getTrackbarPos("AMax", "SelectLAB");
+			bMax = getTrackbarPos("BMax", "SelectLAB");
+
+			minLab = Scalar(LMin, aMin, bMin);
+			maxLab = Scalar(LMax, aMax, bMax);
+
+			// Get values from the YCrCb trackbar
+			YMin = getTrackbarPos("YMin", "SelectYCB");
+			CrMin = getTrackbarPos("CrMin", "SelectYCB");
+			CbMin = getTrackbarPos("CbMin", "SelectYCB");
+
+			YMax = getTrackbarPos("YMax", "SelectYCB");
+			CrMax = getTrackbarPos("CrMax", "SelectYCB");
+			CbMax = getTrackbarPos("CbMax", "SelectYCB");
+
+			minYCrCb = Scalar(YMin, CrMin, CbMin);
+			maxYCrCb = Scalar(YMax, CrMax, CbMax);
+
+			// Convert the BGR image to other color spaces
+			original.copyTo(imageBGR);
+			cvtColor(original, imageHSV, COLOR_BGR2HSV);
+			cvtColor(original, imageYCrCb, COLOR_BGR2YCrCb);
+			cvtColor(original, imageLab, COLOR_BGR2Lab);
+
+			// Create the mask using the min and max values obtained from trackbar and apply bitwise and operation to get the results
+			inRange(imageBGR, minBGR, maxBGR, maskBGR);
+			resultBGR = Mat::zeros(original.rows, original.cols, CV_8UC3);
+			bitwise_and(original, original, resultBGR, maskBGR);
+
+			inRange(imageHSV, minHSV, maxHSV, maskHSV);
+			resultHSV = Mat::zeros(original.rows, original.cols, CV_8UC3);
+			bitwise_and(original, original, resultHSV, maskHSV);
+
+			inRange(imageYCrCb, minYCrCb, maxYCrCb, maskYCrCb);
+			resultYCrCb = Mat::zeros(original.rows, original.cols, CV_8UC3);
+			bitwise_and(original, original, resultYCrCb, maskYCrCb);
+
+			inRange(imageLab, minLab, maxLab, maskLab);
+			resultLab = Mat::zeros(original.rows, original.cols, CV_8UC3);
+			bitwise_and(original, original, resultLab, maskLab);
+
+			// Show the results
+			imshow("SelectBGR", resultBGR);
+			imshow("SelectYCB", resultYCrCb);
+			imshow("SelectLAB", resultLab);
+			imshow("SelectHSV", resultHSV);
 		}
 	}
+	destroyAllWindows();
 	return 0;
 }
