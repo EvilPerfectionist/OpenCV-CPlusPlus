a1be311b7347f56dfd37de2c0d5735504545fa25
EvilPerfectionist <EvilPerfectionist@users.noreply.github.com>
2017-12-09 10:07:39 +0800

    2017.9.12.10.07

--- TestOpenCV/TestOpenCV/Test.cpp
+++ TestOpenCV/TestOpenCV/Test.cpp
@@ -1,11 +1,57 @@
-/* sprintf example */
-#include <stdio.h>
+#include "opencv2/opencv.hpp"
+#include <iostream>
+using namespace cv;
+using namespace std;
 
-int main()
+//Global Variables
+Mat img, placeholder;
+
+
+int main(int argc, const char** argv)
 {
-	char buffer[50];
-	int n, a = 5, b = 3;
-	n = sprintf_s(buffer, "%02d plus %02d is %02d", a, b, a + b);
-	printf("[%s] is a string %d chars long\n", buffer, n);
+	// filename
+	// Read the input image
+	int image_number = 2;
+	int nImages = 10;
+
+	char filename[20];
+	sprintf_s(filename, "images/rub%02d.jpg", image_number%nImages);
+	img = imread(filename);
+	// Resize the image to 400x400
+	Size rsize(400, 400);
+	resize(img, img, rsize);
+
+	/*if (img.empty())
+	{
+		return -1;
+	}*/
+
+	// Create an empty window
+	namedWindow("PRESS P for Previous, N for Next Image", WINDOW_AUTOSIZE);
+	// Create a callback function for any event on the mouse
+
+	imshow("PRESS P for Previous, N for Next Image", img);
+	while (1)
+	{
+		char k = waitKey(1) & 0xFF;
+		if (k == 27)
+			break;
+		//Check next image in the folder
+		if (k == 'n')
+		{
+			image_number++;
+			sprintf_s(filename, "images/rub%02d.jpg", image_number%nImages);
+			img = imread(filename);
+			resize(img, img, rsize);
+		}
+		//Check previous image in he folder
+		else if (k == 'p')
+		{
+			image_number--;
+			sprintf_s(filename, "images/rub%02d.jpg", image_number%nImages);
+			img = imread(filename);
+			resize(img, img, rsize);
+		}
+	}
 	return 0;
 }
